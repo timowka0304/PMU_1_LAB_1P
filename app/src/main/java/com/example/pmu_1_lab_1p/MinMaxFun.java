6a977a6385da4f8284dd5881644cd1898e50f209
timowka0304 <timoti20@mail.ru>
2021-09-10 14:18:42 +0700

    Add MinMax class

---
+++ app/src/main/java/com/example/pmu_1_lab_1p/MinMaxFun.java
@@ -0,0 +1,14 @@
+package com.example.pmu_1_lab_1p;
+
+public class MinMaxFun {
+
+    public int max(int a, int b)
+    {
+        return a > b ? a : b;
+    }
+
+    public int min(int a, int b)
+    {
+        return a > b ? b : a;
+    }
+}
